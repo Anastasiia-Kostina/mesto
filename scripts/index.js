850eddaa20e148d51cc1a309d05f56bdf6d0c169
Anastasiia Kostina <Kaliarmon@yandex.ru>
2023-02-26 00:00:19 +0300

    feature: template cards, delete btn, adapt js

--- scripts/index.js
+++ scripts/index.js
@@ -46,13 +46,83 @@ popupCloseBtn.addEventListener('click', closePopup);
 popupElement.addEventListener('click', handleClosePopupOverlayClick);
 popupFormSubmit.addEventListener('click', handlePopupSubmit);//mb will be 'submit' when complete
 
+
+
+//--------------------template cards start----------------
+const picGalleryCards = document.querySelector('.pic-gallery__cards');
+const picGalleryCardTemplate = document.querySelector('.pic-gallery__cards-template').content;
+
+const initialPicCards = [
+  {
+    title: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+    alt: 'Изображение с замёрзшим Байкалом'
+  },
+  {
+    title: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+    alt: 'Изображение с дорогой в сельской местности Холмогорского района'
+  },
+  {
+    title: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+    alt: 'Изображение с горой на Камчатке'
+  },
+  {
+    title: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+    alt: 'Изображение с кварталом домов в Иваново'
+  },
+  {
+    title: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+    alt: 'Изображение с заснеженными горами и озером Челябинска'
+  },
+  {
+    title: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+    alt: 'Изображение с холмами Архыза'
+  }
+]; 
+
+initialPicCards.forEach(function (element) {
+  const picGalleryCardElement = picGalleryCardTemplate.cloneNode(true);
+  
+  picGalleryCardElement.querySelector('.pic-gallery__delete-button').addEventListener('click', function(event){
+    console.log();
+  });
+  picGalleryCardElement.querySelector('.pic-gallery__title').textContent = element.title;
+  picGalleryCardElement.querySelector('.pic-gallery__image').src = element.link;
+  picGalleryCardElement.querySelector('.pic-gallery__image').alt = element.alt;
+  picGalleryCardElement.querySelector('.pic-gallery__like-button').addEventListener('click', function(event){
+    event.target.classList.toggle('pic-gallery__like-button_active');
+  });
+
+  picGalleryCards.prepend(picGalleryCardElement)
+});
+
+
+//--------------------function delete button start------------
+const deleteButton = document.querySelector('.pic-gallery__delete-button');
+
+deleteButton.forEach(function(element) {
+  element.addEventListener('click', function(event){
+    /*event.target.classList.toggle('pic-gallery__like-button_active');*/
+  });
+});
+
+//------------------function delete button end---------------
+
 //---------------------function like button start-------------------
 
-const likeBtn = document.querySelectorAll('.pic-gallery__like-button');
+/*const likeBtn = document.querySelectorAll('.pic-gallery__like-button');
 
 likeBtn.forEach(function(element) {
   element.addEventListener('click', function(event){
     event.target.classList.toggle('pic-gallery__like-button_active');
-  })
-});
+  });
+});*/
+
 //---------------------function like button end------------------
+
+//--------------------template cards end------------------
