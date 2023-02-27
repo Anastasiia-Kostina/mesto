6d98ff3b56ae7e46c01d277d2ca5a03035a58f88
Anastasiia Kostina <Kaliarmon@yandex.ru>
2023-02-27 13:11:33 +0300

    fix: popup for add and edit, they work separately, didn't touch submit

--- scripts/index.js
+++ scripts/index.js
@@ -1,68 +1,117 @@
 
 //---------------popup profile edit start----------------------- 
 const popupElement = document.querySelector('.popup');
-const popupCloseBtn = popupElement.querySelector('.popup__close-button');
+const popupCloseBtnEdit = popupElement.querySelector('.popup__close-button_edit');//was without edit// changed into document //changed back
 const editButton = document.querySelector('.profile__edit-button');
 
-const popupFormSubmitEdit = popupElement.querySelector(".popup__submit-edit-button");
+const popupFormSubmitEdit = popupElement.querySelector(".popup__submit-edit-button");//changed into document//changed back
 const popupEdit = document.querySelector(".popup_edit");
-const popupInputName = popupElement.querySelector('.popup__input_type_name');
-const popupInputOccupation = popupElement.querySelector('.popup__input_type_occupation');
+const popupInputName = popupElement.querySelector('.popup__input_type_name');//changed into document//changed back
+const popupInputOccupation = popupElement.querySelector('.popup__input_type_occupation');//changed into document//changed back
 const profileInfoName = document.querySelector('.profile__info-name');
 const profileInfoOccupation = document.querySelector('.profile__info-occupation');
+//---------------popup profile add start-----------------------
+const addCardButton = document.querySelector('.profile__add-button');
+const popupCloseBtnAdd = document.querySelector('.popup__close-button_add'); //forgot to change into document here popupElement doesn't work
+const popupFormSubmitCreate = popupElement.querySelector(".popup__submit-create-button"); //changed into document//changed back
+const popupAdd = document.querySelector(".popup_add");
+const popupInputCardName = popupElement.querySelector('.popup__input_type_card-pic-name');//changed into document//changed back
+const popupInputCardLink = popupElement.querySelector('.popup__input_type_card-pic-link');//changed into document//changed back
+//---------------popup profile add end-----------------------
 
 
-const openPopup = function(event) {
+//---------------popups----------
+const openPopupEdit = (event) => {
   popupEdit.classList.add('popup_opened');
   popupInputName.value = profileInfoName.textContent;
   popupInputOccupation.value = profileInfoOccupation.textContent;
-};
+} 
 
-const closePopup = function() {
+const openPopupAdd = (event) => {
+  popupAdd.classList.add('popup_opened');
+} 
+
+
+
+//-------works but from the second click---------
+/*const openPopup = (event) => {
+
+  editButton.onclick = function () {
+  popupEdit.classList.add('popup_opened');
+  popupInputName.value = profileInfoName.textContent;
+  popupInputOccupation.value = profileInfoOccupation.textContent;
+}
+  addCardButton.onclick = function () {
+  popupAdd.classList.add('popup_opened');
+}
+};*/
+
+//----------close buttons------------
+const closePopupEdit = (event) => {
   popupEdit.classList.remove('popup_opened');
 };
 
+const closePopupAdd = (event) => {
+  popupAdd.classList.remove('popup_opened');
+};
 
-const handleClosePopupOverlayClick = function (event) {
+const handleClosePopupOverlayClick = (event) => {
   if (event.target === event.currentTarget) {
-    closePopup();
+    closePopupEdit(); //was closePopup
+    closePopupAdd();
   }
   else {
     return;
   }
 }
 
+//------------close buttons----------
+
+
+//----------OLD close works for edit-----------
+
+/*const closePopup = (event) => {
+  popupEdit.classList.remove('popup_opened');
+};
+
+
+const handleClosePopupOverlayClick = (event) => {
+  if (event.target === event.currentTarget) {
+    closePopup();
+  }
+  else {
+    return;
+  }
+}*/
+
 //-----------------------function popup edit start----------------------
 const handlePopupSubmit = function (event) {
   event.preventDefault();
   profileInfoName.textContent = popupInputName.value;
   profileInfoOccupation.textContent = popupInputOccupation.value;
-  closePopup();
+  closePopupEdit(); //was closePopup
 }
 //----------------------function popup edit end-------------------
 
-editButton.addEventListener('click', openPopup);
-popupCloseBtn.addEventListener('click', closePopup);
-popupElement.addEventListener('click', handleClosePopupOverlayClick);
+editButton.addEventListener('click', openPopupEdit); //was openPopup
+popupCloseBtnEdit.addEventListener('click', closePopupEdit);//was closePopup
+//popupElement.addEventListener('click', handleClosePopupOverlayClick); //deleted b/c popupCloseBtnAdd wouldn't work
 popupFormSubmitEdit.addEventListener('click', handlePopupSubmit); //mb will be 'submit' when complete
 //---------------popup profile edit end-----------------------
 
+
 //---------------popup profile add start-----------------------
-const addCardButton = document.querySelector('.profile__add-button');
-const popupFormSubmitCreate = popupElement.querySelector(".popup__submit-create-button");
-const popupAdd = document.querySelector(".popup_add");
-const popupInputCardName = popupElement.querySelector('.popup__input_type_card-pic-name');
-const popupInputCardLink = popupElement.querySelector('.popup__input_type_card-pic-link');
+addCardButton.addEventListener('click', openPopupAdd);
+popupCloseBtnAdd.addEventListener('click', closePopupAdd);
 
-
-
-
-
+popupEdit.addEventListener('click', handleClosePopupOverlayClick);
+popupAdd.addEventListener('click', handleClosePopupOverlayClick);
 //---------------popup profile add end-----------------------
+
 
 //--------------------template cards start------------------
 
-const picGalleryCards = document.querySelector('.pic-gallery__cards');
+const picGalleryCards = document.querySelector('.pic-gallery__cards'); //was openPopup
 const picGalleryCardTemplate = document.querySelector('.pic-gallery__cards-template').content;
 
 const initialPicCards = [
