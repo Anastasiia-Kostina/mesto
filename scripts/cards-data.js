93bdc80e65eadc1032be51c2bec419967d7bae66
Anastasiia Kostina <Kaliarmon@yandex.ru>
2023-03-02 15:06:33 +0300

    fix: create cards-data file for the cards

---
+++ scripts/cards-data.js
@@ -0,0 +1,32 @@
+const initialPicCards = [
+  {
+    name: 'Санкт-Петербург',
+    link: 'https://images.unsplash.com/photo-1601580047389-b538f04850c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzYyfHwlRDAlQTElRDAlQjAlRDAlQkQlRDAlQkElRDElODIlMjAlRDAlOUYlRDAlQjUlRDElODIlRDAlQjUlRDElODAlRDAlQjElRDElODMlRDElODAlRDAlQjN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+    //link: 'https://photocentra.ru/images/main68/680823_main.jpg',
+  },
+  {
+    name: 'Орхус',
+    link: 'https://images.unsplash.com/photo-1558443336-d42ee773809c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    //link: 'https://assets.suitcasemag.com/images/hero_mobile_1400/242054-aarhus-design-1.jpg',
+  },
+  {
+    name: 'Киото',
+    link: 'https://images.unsplash.com/photo-1616360742542-7393fcba7547?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+    //link: 'https://i.pinimg.com/originals/ff/55/16/ff55164145660cf2e4fb8cc7487f8d29.jpg',
+  },
+  {
+    name: 'Берлин',
+    link: 'https://images.unsplash.com/photo-1569164395803-6e78445864be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    //link: 'https://cms.enjourney.ru/upload/Jana/Deutschland/Berlin%20city/Berlin15.jpg',
+  },
+  {
+    name: 'Нью-Йорк',
+    link: 'https://images.unsplash.com/photo-1484102436255-9f89131e21f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+    //link: 'https://avatars.dzeninfra.ru/get-zen_doc/3362051/pub_619f2fda6a45596de8b07fa8_619f304145832129f9ffc98e/scale_1200',
+  },
+  {
+    name: 'Милан',
+    link: 'https://images.unsplash.com/photo-1512236077335-f1cda9239c11?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', 
+    //link: 'https://i.imgur.com/nPf37vB.jpg?1', 
+  }
+]; 
