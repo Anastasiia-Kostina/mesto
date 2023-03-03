7022353fa293e77edc0dc7ff46318b2d40b948ea
Anastasiia Kostina <Kaliarmon@yandex.ru>
2023-03-03 20:04:10 +0300

    prettier

--- scripts/index.js
+++ scripts/index.js
@@ -1,68 +1,83 @@
-
-//---------------popup profile edit const start----------------------- 
+//---------------popup profile edit const start-----------------------
 const btnEditProfile = document.querySelector(".profile__edit-button");
 
-const popupCloseBtnEditProfile = document.querySelector(".popup__close-button-edit");
-const popupFormSubmitEditProfile = document.querySelector(".popup__form_edit-profile");
+const popupCloseBtnEditProfile = document.querySelector(
+  ".popup__close-button-edit"
+);
+const popupFormSubmitEditProfile = document.querySelector(
+  ".popup__form_edit-profile"
+);
 const popupEditProfile = document.querySelector(".popup_edit");
 const popupInputName = document.querySelector(".popup__input_type_name");
-const popupInputOccupation = document.querySelector(".popup__input_type_occupation");
+const popupInputOccupation = document.querySelector(
+  ".popup__input_type_occupation"
+);
 
 const profileInfoName = document.querySelector(".profile__info-name");
-const profileInfoOccupation = document.querySelector(".profile__info-occupation");
-//---------------popup profile edit const end----------------------- 
+const profileInfoOccupation = document.querySelector(
+  ".profile__info-occupation"
+);
+//---------------popup profile edit const end-----------------------
 
 //---------------popup profile add const start-----------------------
 const btnAddCard = document.querySelector(".profile__add-button");
 
-const popupCloseBtnAddCard = document.querySelector(".popup__close-button-add"); 
+const popupCloseBtnAddCard = document.querySelector(".popup__close-button-add");
 const popupFormSubmitAddCard = document.querySelector(".popup__form_add-card");
 const popupAddCard = document.querySelector(".popup_add");
-const popupInputCardName = document.querySelector(".popup__input_type_card-name");
-const popupInputCardLink = document.querySelector(".popup__input_type_card-link");
+const popupInputCardName = document.querySelector(
+  ".popup__input_type_card-name"
+);
+const popupInputCardLink = document.querySelector(
+  ".popup__input_type_card-link"
+);
 //---------------popup profile add const end-----------------------
 
 //-------------popup-pic const start--------------------
 const popupForZoomPic = document.querySelector(".popup-picture");
-const popupCloseBtnZoomPic = document.querySelector(".popup-picture__close-button");
+const popupCloseBtnZoomPic = document.querySelector(
+  ".popup-picture__close-button"
+);
 const popupPicZoom = document.querySelector(".popup-picture__zoom");
 const popupPicName = document.querySelector(".popup-picture__zoom-name");
 //-------------popup-pic const end---------------------
 
-//-------------createCard const start----------------- 
+//-------------createCard const start-----------------
 const picGalleryCardsContainer = document.querySelector(".pic-gallery__cards");
-const picGalleryCardTemplate = document.querySelector(".pic-gallery__cards-template").content; //Couldn't find the right way to change it
+const picGalleryCardTemplate = document.querySelector(
+  ".pic-gallery__cards-template"
+).content; //Couldn't find the right way to change it
 //------------create const end------------------------
 
 //---------------popups open-close start----------
-function openPopup (openPopupArea) {
- openPopupArea.classList.add('popup-picture_opened');//DKW, but it doesn't work with popup_opened. I think it's about BEM...
+function openPopup(openPopupArea) {
+  openPopupArea.classList.add("popup-picture_opened"); //DKW, but it doesn't work with popup_opened. I think it's about BEM...
 }
 
-function closePopup (closePopupArea) {
- closePopupArea.classList.remove('popup-picture_opened');//DKW, but it doesn't work with popup_opened. I think it's about BEM...
+function closePopup(closePopupArea) {
+  closePopupArea.classList.remove("popup-picture_opened"); //DKW, but it doesn't work with popup_opened. I think it's about BEM...
 }
 
 const openPopupEditProfile = (event) => {
   popupInputName.value = profileInfoName.textContent;
   popupInputOccupation.value = profileInfoOccupation.textContent;
-  openPopup (popupEditProfile);
-} 
+  openPopup(popupEditProfile);
+};
 
 const openPopupAddCard = (event) => {
-  openPopup (popupAddCard);
-} 
+  openPopup(popupAddCard);
+};
 
 const openPopupForZoomPic = (event) => {
   openPopup(popupForZoomPic);
-}
+};
 
 const closePopupEditProfile = (event) => {
-  closePopup (popupEditProfile);
+  closePopup(popupEditProfile);
 };
 
 const closePopupAddCard = (event) => {
-  closePopup (popupAddCard);
+  closePopup(popupAddCard);
 };
 
 const closePopupForZoomPic = (event) => {
@@ -72,11 +87,10 @@ const closePopupForZoomPic = (event) => {
 const handleClosePopupOverlayClick = (event) => {
   if (event.target === event.currentTarget) {
     closePopup(event.currentTarget);
-  }
-  else {
+  } else {
     return;
   }
-}
+};
 //---------------popups open-close end----------
 
 //-----------------------function popup edit start----------------------
@@ -85,7 +99,7 @@ const handlePopupSubmitEditProfile = (event) => {
   profileInfoName.textContent = popupInputName.value;
   profileInfoOccupation.textContent = popupInputOccupation.value;
   closePopupEditProfile();
-}
+};
 //----------------------function popup edit end-------------------
 
 //--------------------template cards start------------------
@@ -95,43 +109,52 @@ const handlePopupSubmitAddCard = (event) => {
   console.log(event.defaultPrevented);
   const cardEntry = {
     name: popupInputCardName.value,
-    link: popupInputCardLink.value
+    link: popupInputCardLink.value,
   };
   picGalleryCardsContainer.prepend(createCard(cardEntry));
   closePopupAddCard();
   event.target.reset();
-}
+};
 //------------Add handler end-------------------------------
 
 //-----------Add function that creates a card start-------
 const createCard = (cardData) => {
-  const picGalleryCardElement = picGalleryCardTemplate.querySelector(".pic-gallery__card").cloneNode(true); //Not sure if that change was meant (remarque 10)
-  const picGalleryCardElementPic = picGalleryCardElement.querySelector(".pic-gallery__image");
-  const picGalleryCardElementName = picGalleryCardElement.querySelector(".pic-gallery__name");
+  const picGalleryCardElement = picGalleryCardTemplate
+    .querySelector(".pic-gallery__card")
+    .cloneNode(true); //Not sure if that change was meant (remarque 10)
+  const picGalleryCardElementPic = picGalleryCardElement.querySelector(
+    ".pic-gallery__image"
+  );
+  const picGalleryCardElementName =
+    picGalleryCardElement.querySelector(".pic-gallery__name");
 
-  picGalleryCardElement.querySelector(".pic-gallery__delete-button").addEventListener('click', function(event){
-    picGalleryCardElement.remove();
-  });
+  picGalleryCardElement
+    .querySelector(".pic-gallery__delete-button")
+    .addEventListener("click", function (event) {
+      picGalleryCardElement.remove();
+    });
 
   picGalleryCardElementPic.src = cardData.link;
   picGalleryCardElementName.textContent = cardData.name;
   picGalleryCardElementPic.alt = cardData.name;
 
-  picGalleryCardElement.querySelector(".pic-gallery__like-button").addEventListener('click', handleLikeBtn);
+  picGalleryCardElement
+    .querySelector(".pic-gallery__like-button")
+    .addEventListener("click", handleLikeBtn);
 
-  picGalleryCardElementPic.addEventListener('click', renderPopupForZoomPic);
+  picGalleryCardElementPic.addEventListener("click", renderPopupForZoomPic);
 
   return picGalleryCardElement;
-}
+};
 //-------------like button function start---------------
-function handleLikeBtn (event) {
-event.target.classList.toggle("pic-gallery__like-button_active");
+function handleLikeBtn(event) {
+  event.target.classList.toggle("pic-gallery__like-button_active");
 }
 //-------------like button function end-----------------
 
 //--------------forEach for cards start-----------------
 initialPicCards.forEach((element) => {
-  picGalleryCardsContainer.prepend(createCard(element))
+  picGalleryCardsContainer.prepend(createCard(element));
 });
 //--------------forEach for cards end-------------------
 
@@ -147,17 +170,20 @@ function renderPopupForZoomPic(event) {
 //--------------------template cards end------------------
 
 //----------Listeners start----------------------------
-btnEditProfile.addEventListener('click', openPopupEditProfile);
-popupCloseBtnEditProfile.addEventListener('click', closePopupEditProfile);
-popupFormSubmitEditProfile.addEventListener('submit', handlePopupSubmitEditProfile);
+btnEditProfile.addEventListener("click", openPopupEditProfile);
+popupCloseBtnEditProfile.addEventListener("click", closePopupEditProfile);
+popupFormSubmitEditProfile.addEventListener(
+  "submit",
+  handlePopupSubmitEditProfile
+);
 
-btnAddCard.addEventListener('click', openPopupAddCard);
-popupCloseBtnAddCard.addEventListener('click', closePopupAddCard);
-popupFormSubmitAddCard.addEventListener('submit', handlePopupSubmitAddCard);
+btnAddCard.addEventListener("click", openPopupAddCard);
+popupCloseBtnAddCard.addEventListener("click", closePopupAddCard);
+popupFormSubmitAddCard.addEventListener("submit", handlePopupSubmitAddCard);
 
-popupEditProfile.addEventListener('click', handleClosePopupOverlayClick);
-popupAddCard.addEventListener('click', handleClosePopupOverlayClick);
-popupForZoomPic.addEventListener('click', handleClosePopupOverlayClick);
+popupEditProfile.addEventListener("click", handleClosePopupOverlayClick);
+popupAddCard.addEventListener("click", handleClosePopupOverlayClick);
+popupForZoomPic.addEventListener("click", handleClosePopupOverlayClick);
 
-popupCloseBtnZoomPic.addEventListener('click', closePopupForZoomPic);
+popupCloseBtnZoomPic.addEventListener("click", closePopupForZoomPic);
 //----------Listeners end----------------------------
