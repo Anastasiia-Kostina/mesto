8211d3b66fd3f77c97def430a8e7f1d04033f93e
Anastasiia Kostina <Kaliarmon@yandex.ru>
2023-03-10 19:08:10 +0300

    refactor: cards, message, comments

--- scripts/cards-data.js
+++ scripts/cards-data.js
@@ -1,32 +1,38 @@
 const initialPicCards = [
   {
     name: 'Санкт-Петербург',
-    link: 'https://images.unsplash.com/photo-1601580047389-b538f04850c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzYyfHwlRDAlQTElRDAlQjAlRDAlQkQlRDAlQkElRDElODIlMjAlRDAlOUYlRDAlQjUlRDElODIlRDAlQjUlRDElODAlRDAlQjElRDElODMlRDElODAlRDAlQjN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-    //link: 'https://photocentra.ru/images/main68/680823_main.jpg',
+    link: 'https://images.unsplash.com/photo-1603130392453-6ced0007cc57?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    /*name: 'Санкт-Петербург',
+    link: 'https://images.unsplash.com/photo-1601580047389-b538f04850c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzYyfHwlRDAlQTElRDAlQjAlRDAlQkQlRDAlQkElRDElODIlMjAlRDAlOUYlRDAlQjUlRDElODIlRDAlQjUlRDElODAlRDAlQjElRDElODMlRDElODAlRDAlQjN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',*/
   },
   {
-    name: 'Орхус',
-    link: 'https://images.unsplash.com/photo-1558443336-d42ee773809c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-    //link: 'https://assets.suitcasemag.com/images/hero_mobile_1400/242054-aarhus-design-1.jpg',
+    name: 'Териберка',
+    link: 'https://images.unsplash.com/photo-1606841002501-4eebe03697ed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    /*name: 'Орхус',
+    link: 'https://images.unsplash.com/photo-1558443336-d42ee773809c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',*/
   },
   {
-    name: 'Киото',
-    link: 'https://images.unsplash.com/photo-1616360742542-7393fcba7547?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
-    //link: 'https://i.pinimg.com/originals/ff/55/16/ff55164145660cf2e4fb8cc7487f8d29.jpg',
+    name: 'Куршская коса',
+    link: 'https://images.unsplash.com/photo-1612639959705-20615d6cf416?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+    /*name: 'Киото',
+    link: 'https://images.unsplash.com/photo-1616360742542-7393fcba7547?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',*/
   },
   {
-    name: 'Берлин',
-    link: 'https://images.unsplash.com/photo-1569164395803-6e78445864be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-    //link: 'https://cms.enjourney.ru/upload/Jana/Deutschland/Berlin%20city/Berlin15.jpg',
+    name: 'Сортавала',
+    link: 'https://images.unsplash.com/photo-1662804118977-481122aa1d10?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+    /*name: 'Берлин',
+    link: 'https://images.unsplash.com/photo-1569164395803-6e78445864be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',*/
   },
   {
-    name: 'Нью-Йорк',
-    link: 'https://images.unsplash.com/photo-1484102436255-9f89131e21f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
-    //link: 'https://avatars.dzeninfra.ru/get-zen_doc/3362051/pub_619f2fda6a45596de8b07fa8_619f304145832129f9ffc98e/scale_1200',
+    name: 'Бадукские озёра',
+    link: 'https://images.unsplash.com/photo-1634665605165-cf4a1635503e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80',
+    /*name: 'Нью-Йорк',
+    link: 'https://images.unsplash.com/photo-1484102436255-9f89131e21f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',*/
   },
   {
-    name: 'Милан',
-    link: 'https://images.unsplash.com/photo-1512236077335-f1cda9239c11?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', 
-    //link: 'https://i.imgur.com/nPf37vB.jpg?1', 
+    name: 'Курильские острова',
+    link: 'https://images.unsplash.com/photo-1647391342641-f18cf2994f73?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80',
+    /*name: 'Милан',
+    link: 'https://images.unsplash.com/photo-1512236077335-f1cda9239c11?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',*/ 
   }
 ]; 
